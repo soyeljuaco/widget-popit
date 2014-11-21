f7d2d7246748730dabb473de044869a35c16ccc6
Joaquin Figueroa <joaquin@forkhq.com>
2014-11-21 14:40:04 -0300

    front styles

--- popit_front.php
+++ popit_front.php
@@ -1,5 +1,32 @@
-<div class="cha_fichas">
-   <img src="<?= $aPerson->result->images[0]->url ?>">
-   <span><?= $aPerson->result->name ?></span>
-   <h4><?= $aPerson->result->personal_info[0]->profession;?></h4>
+
+<div class="ficha">
+   <img class="img-bio" src="<?= $aPerson->result->images[0]->url ?>">
+   <div class="description">
+	   <span class="name"><?= $aPerson->result->name ?></span>
+	   <h4 class="profession"><?= $aPerson->result->personal_info[0]->profession;?></h4>
+   </div>
 </div>
+
+
+
+<style type="text/css">
+	.ficha{
+		width: 100%;
+		margin: 30px auto;
+		text-align: center;
+	}
+
+	span.name{
+		font-size: 20px;
+
+	}
+
+	h4{
+		margin: 0;
+		padding: 0;
+	}
+
+	.img-bio{
+
+	}
+</style>
